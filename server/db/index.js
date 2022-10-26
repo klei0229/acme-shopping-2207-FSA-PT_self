95ce42c455512c5f3c16b3dade166a6d18850b78
ericpkatz <ericpkatz@gmail.com>
2022-10-25 20:42:53 -0400

    set up authentication methods

---
+++ server/db/index.js
@@ -0,0 +1,88 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db');
+const { STRING, UUID, UUIDV4 } = Sequelize;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT = process.env.JWT;
+
+
+const User = conn.define('user', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+    unique: true
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
+User.addHook('beforeSave', async(user)=> {
+  if(user.changed('password')){
+    user.password = await bcrypt.hash(user.password, 5);
+  }
+});
+
+User.findByToken = async function(token){
+  try {
+    const { id } = jwt.verify(token, process.env.JWT);
+    const user = await this.findByPk(id);
+    if(user){
+      return user;
+    }
+    throw 'user not found';
+  }
+  catch(ex){
+    const error = new Error('bad credentials');
+    error.status = 401;
+    throw error;
+  }
+}
+
+User.authenticate = async function({ username, password }){
+  const user = await this.findOne({
+    where: {
+      username
+    }
+  });
+  if(user && await bcrypt.compare(password, user.password)){
+    return jwt.sign({ id: user.id }, JWT);
+  }
+  const error = new Error('bad credentials');
+  error.status = 401;
+  throw error;
+}
+
+const syncAndSeed = async()=> {
+  await conn.sync({ force: true });
+  const [moe, lucy, larry] = await Promise.all([
+    User.create({ username: 'moe', password: '123' }),
+    User.create({ username: 'lucy', password: '123' }),
+    User.create({ username: 'larry', password: '123' }),
+  ]);
+  return {
+    users: {
+      moe,
+      lucy,
+      larry
+    }
+  };
+};
+
+
+module.exports = {
+  syncAndSeed,
+  User
+};
---
+++ test/spec.js
@@ -0,0 +1,65 @@
+const { expect } = require('chai');
+const { syncAndSeed, User } = require('../server/db');
+const jwt = require('jsonwebtoken');
+describe('The Login Process', ()=> {
+  let seed;
+  beforeEach(async()=> {
+    seed = await syncAndSeed();
+  });
+  describe('password storage', ()=> {
+    it('password is hashed', ()=> {
+      expect(seed.users.moe.password).not.to.equal('123');
+    });
+    it('doesnt get rehashed when a user updates', async()=> {
+      const { moe } = seed.users;
+      const password = moe.password;
+      moe.username = 'MOE';
+      await moe.save();
+      expect(password).to.equal(moe.password);
+    });
+  });
+  describe('with correct credentials', ()=> {
+    it('returns a jwt token', async()=> {
+      const token = await User.authenticate({
+        username: 'lucy',
+        password: '123'
+      });
+      const { id } = jwt.verify(token, process.env.JWT);
+      expect(seed.users.lucy.id).to.equal(id);
+    });
+  });
+  describe('token exchange', ()=> {
+    describe('with a valid token', ()=> {
+      it('can be exchange for the user', async()=> {
+        const token = jwt.sign({ id: seed.users.larry.id }, process.env.JWT);
+        const larry = await User.findByToken(token);
+        expect(larry.username).to.equal('larry');
+      });
+      describe('with no user', ()=> {
+        it('will throw an error', async()=> {
+          const token = jwt.sign({ id: seed.users.larry.id }, process.env.JWT);
+          await seed.users.larry.destroy();
+          try {
+            const larry = await User.findByToken(token);
+            throw 'noooooooo';
+          }
+          catch(ex){
+            expect(ex.status).to.equal(401);
+          }
+        });
+      });
+    });
+    describe('with a invalid token', ()=> {
+      it('can not be exchanged for the user', async()=> {
+        const token = jwt.sign({ id: seed.users.larry.id }, process.env.JWT + '!');
+        try {
+          const larry = await User.findByToken(token);
+          throw 'noooooooo';
+        }
+        catch(ex){
+          expect(ex.status).to.equal(401);
+        }
+      });
+    });
+  });
+});
