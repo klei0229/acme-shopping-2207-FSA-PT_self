a964f3d1e075523aaa12d26519502953d58ed58c
Beejay Salting <beejay.salting@gmail.com>
2022-10-29 11:56:04 -0400

    beejay ci test

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,10 +1,14 @@
-const Sequelize = require('sequelize');
-const config = {
-};
+const Sequelize = require("sequelize");
+const config = {};
 
-if(process.env.QUIET){
+if (process.env.QUIET) {
   config.logging = false;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_shopping_db",
+  config
+);
 
 module.exports = conn;
+
+//ci test
