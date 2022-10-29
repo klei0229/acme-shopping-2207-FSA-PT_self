f8e3d93629babed3804f1bdc8bd8f4d65de6bcf8
Beejay Salting <beejay.salting@gmail.com>
2022-10-29 12:00:22 -0400

    ci test

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Sequelize = require("sequelize");
 const config = {};
 
@@ -8,23 +7,8 @@ if (process.env.QUIET) {
 const conn = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost/acme_shopping_db",
   config
-=======
-const Sequelize = require('sequelize');
-const config = {};
-
-if (process.env.QUIET) {
-	config.logging = false;
-}
-const conn = new Sequelize(
-	process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db',
-	config
->>>>>>> 9f7321885faf8bd46f16f5f6a668f7ebb284403c
 );
 
 module.exports = conn;
 
-<<<<<<< HEAD
-//ci test
-=======
-// Ethan's comment
->>>>>>> 9f7321885faf8bd46f16f5f6a668f7ebb284403c
+//beejay test
