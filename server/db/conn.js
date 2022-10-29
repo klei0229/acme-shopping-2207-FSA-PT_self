8377a7e946de26ff0843b05386a45d78445d07ea
Ethan Newlin <newlin5@gmail.com>
2022-10-29 12:17:16 -0400

    fixed

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,14 +1,3 @@
-
-const Sequelize = require("sequelize");
-const config = {};
-
-if (process.env.QUIET) {
-  config.logging = false;
-}
-const conn = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/acme_shopping_db",
-  config
-
 const Sequelize = require('sequelize');
 const config = {};
 
@@ -18,7 +7,6 @@ if (process.env.QUIET) {
 const conn = new Sequelize(
 	process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db',
 	config
-
 );
 
 module.exports = conn;
@@ -26,13 +14,8 @@ module.exports = conn;
 //Fernando's comment
 // Ethan's comment
 
-
-
-
 //beejay test try 2
 
 // Ethan's comment
 
-
-//Kevin's comment 
-
+//Kevin's comment
