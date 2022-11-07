6eb1f3fd93e62358d8e345f31e7d025e578a1554
Beejay Salting <beejay.salting@gmail.com>
2022-11-06 22:23:04 -0500

    new user login and cleanup

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,42 +10,41 @@ import { loginWithToken, fetchCart, fetchBundles } from '../store';
 import { Link, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-	const { auth } = useSelector((state) => state);
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(loginWithToken());
-		dispatch(fetchBundles());
-	}, []);
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loginWithToken());
+    dispatch(fetchBundles());
+  }, []);
 
-	useEffect(() => {
-		if (auth.id) {
-			dispatch(fetchCart());
-		}
-	}, [auth]);
-	return (
-		<div>
-			<h1>Acme Shopping</h1>
-			{auth.id ? <Home /> : <Login />}
-			{!!auth.id && (
-				<div>
-					<nav>
-						<Link to='/'>Home</Link>
-						<Link to='/cart'>Cart</Link>
-						<Link to='/profile'>Profile</Link>
-						<Link to='/checkout'>Checkout</Link>
-						<Link to='/bundles'>Bundles</Link>
-					</nav>
-					<Routes>
-						<Route path='/' element={<h3>Home</h3>} />
-						<Route path='/cart' element={<Cart />} />
-						<Route path='/profile' element={<Profile />} />
-						<Route path='/checkout' element={<Checkout />} />
-						<Route path='/bundles' element={<Bundle />} />
-					</Routes>
-				</div>
-			)}
-		</div>
-	);
+  useEffect(() => {
+    if (auth.id) {
+      dispatch(fetchCart());
+    }
+  }, [auth]);
+  return (
+    <div>
+      {auth.id ? <Home /> : <Login />}
+      {!!auth.id && (
+        <div>
+          {/* <nav>
+            <Link to="/">Home</Link>
+            <Link to="/cart">Cart</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/checkout">Checkout</Link>
+            <Link to="/bundles">Bundles</Link>
+          </nav> */}
+          <Routes>
+            <Route path="/" element={<h3>Home</h3>} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/bundles" element={<Bundle />} />
+          </Routes>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../store';
 
-import ResponsiveAppBar from './Navbar'
+import ResponsiveAppBar from './Navbar';
 
-const Home = ()=> {
-  const { auth } = useSelector(state => state);
+const Home = () => {
+  const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
     <div>
       <ResponsiveAppBar></ResponsiveAppBar>
-      <h1>Home</h1>
+      {/* <h1>Home</h1>
       <div>
         Welcome { auth.username }!!
         <button onClick={()=> dispatch(logout())}>Logout</button>
         {
           !!auth.avatar && <img src={auth.avatar} />
         }
-      </div>
+      </div> */}
     </div>
   );
 };
