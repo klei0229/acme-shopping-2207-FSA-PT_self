3a959500bdfacfc5faf9904e301beada8aeb13cc
Ethan Newlin <newlin5@gmail.com>
2022-11-10 20:38:57 -0500

    small updates

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import { updateAuth } from '../store';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid, Typography, TextField } from '@mui/material';
+import {
+	Grid,
+	Typography,
+	TextField,
+	FormControlLabel,
+	Checkbox,
+} from '@mui/material';
 
 const Profile = () => {
 	const { auth } = useSelector((state) => state);
@@ -15,9 +21,19 @@ const Profile = () => {
 		email: auth.email,
 		avatar: auth.avatar,
 	});
+	const [address, setAddress] = useState({
+		street1: '',
+		street2: '',
+		city: '',
+		state: '',
+		zipcode: '',
+		country: '',
+		isShipping: true,
+	});
 
 	const onChange = (ev) => {
 		setUser({ ...user, [ev.target.name]: ev.target.value });
+		setAddress({ ...address, [ev.target.name]: ev.target.value });
 	};
 
 	useEffect(() => {
@@ -31,7 +47,21 @@ const Profile = () => {
 				});
 			});
 		}
-	}, [el]);
+		if (auth) {
+			const _address = auth.addresses[0];
+			if (_address) {
+				setAddress({
+					street1: _address.street1,
+					street2: _address.street2,
+					city: _address.city,
+					state: _address.state,
+					zipcode: _address.zipcode,
+					country: _address.zipcode,
+					isShipping: _address.isShipping,
+				});
+			}
+		}
+	}, [auth, el]);
 
 	const save = async (ev) => {
 		ev.preventDefault();
@@ -42,7 +72,17 @@ const Profile = () => {
 			email: user.email,
 			avatar: data,
 		};
+		const newAddress = {
+			street1: address.street1,
+			street2: address.street2,
+			city: address.city,
+			state: address.state,
+			zipcode: address.zipcode,
+			country: address.zipcode,
+			isShipping: address.isShipping,
+		};
 		try {
+			console.log(newAddress);
 			await dispatch(updateAuth(newAuth));
 			el.value = '';
 			setData('');
@@ -57,7 +97,7 @@ const Profile = () => {
 					Profile
 				</Typography>
 				<form onSubmit={save}>
-					<Grid container spacing={3}>
+					<Grid container spacing={3} style={{ padding: '0 20px' }}>
 						<Grid item xs={12} sm={6}>
 							<TextField
 								required
@@ -103,12 +143,95 @@ const Profile = () => {
 							/>
 						</Grid>
 						<Grid item xs={12}>
+							<label>Upload a user photo: </label>
 							<input type='file' ref={(x) => setEl(x)} />
 						</Grid>
 					</Grid>
 					<br />
-					<div></div>
-					<button>Update Profile</button>
+					<Typography variant='h6' gutterBottom>
+						Shipping Address
+					</Typography>
+					<div>
+						<Grid container spacing={3}>
+							<Grid item xs={12}>
+								<TextField
+									required
+									name='street1'
+									label='Address line 1'
+									fullWidth
+									variant='standard'
+									value={address.street1}
+									onChange={onChange}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									name='street2'
+									label='Address line 2'
+									fullWidth
+									variant='standard'
+									value={address.street2}
+									onChange={onChange}
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									required
+									name='city'
+									label='City'
+									fullWidth
+									variant='standard'
+									value={address.city}
+									onChange={onChange}
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									name='state'
+									label='State/Province/Region'
+									fullWidth
+									variant='standard'
+									value={address.state}
+									onChange={onChange}
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									required
+									name='zipcode'
+									label='Zipcode / Postal code'
+									fullWidth
+									variant='standard'
+									value={address.zipcode}
+									onChange={onChange}
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									required
+									name='country'
+									label='Country'
+									fullWidth
+									variant='standard'
+									value={address.country}
+									onChange={onChange}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<FormControlLabel
+									control={
+										<Checkbox
+											color='secondary'
+											name='isShipping'
+											value={address.isShipping}
+										/>
+									}
+									label='Use this address for shipping details'
+								/>
+							</Grid>
+						</Grid>
+					</div>
+					<button>Update Profile and Shipping Address</button>
 				</form>
 				<img src={data} />
 			</Fragment>
