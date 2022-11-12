547487d300fb50aa553552ea4e668e980accd0fe
Beejay Salting <beejay.salting@gmail.com>
2022-11-12 14:29:37 -0500

    created new login2 page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Home from './Home';
 import Login from './Login';
+import Login2 from './Login2';
 import Cart from './Cart';
 import Profile from './Profile';
 import Checkout from './Checkout';
@@ -11,7 +12,7 @@ import BundleDetail from './Bundle/BundleDetail';
 import OrderSuccess from './OrderSuccess';
 import OrderFail from './OrderFail';
 import Orders from './Orders';
-import { SnackbarProvider} from 'notistack';
+import { SnackbarProvider } from 'notistack';
 import BundleFeatured from './Bundle/BundleFeatured';
 import BundleNew from './Bundle/BundleNew';
 import BundleBest from './Bundle/BundleBest';
@@ -28,44 +29,44 @@ const App = () => {
     dispatch(fetchBundles());
   }, []);
 
-	useEffect(() => {
-		if (auth.id) {
-			dispatch(fetchCart());
-		}
-	}, [auth]);
-	return (
-		<SnackbarProvider maxSnack={3}>
-		<div>
-			{auth.id ? <Home /> : <Login />}
-			{!!auth.id && (
-				<div>
-					{/* <nav>
+  useEffect(() => {
+    if (auth.id) {
+      dispatch(fetchCart());
+    }
+  }, [auth]);
+  return (
+    <SnackbarProvider maxSnack={3}>
+      <div>
+        {auth.id ? <Home /> : <Login2 />}
+        {!!auth.id && (
+          <div>
+            {/* <nav>
             <Link to="/">Home</Link>
             <Link to="/cart">Cart</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/checkout">Checkout</Link>
             <Link to="/bundles">Bundles</Link>
           </nav> */}
-					<Routes>
-						<Route path='/' element={<LandingPage/>} />
-						<Route path='/cart' element={<Cart />} />
-						<Route path='/profile' element={<Profile />} />
-						<Route path='/checkout' element={<Checkout />} />
-						<Route path='/bundles' element={<Bundle />} />
-						<Route path='/snacks' element={<SnacksMainPage />} />
-						<Route path='/bundles/:id' element={<BundleDetail />} />
-						<Route path='/bundles/featured' element={<BundleFeatured />} />
-						<Route path='/bundles/new' element={<BundleNew />} />
-						<Route path='/bundles/best' element={<BundleBest />} />
-            <Route path="/order-success" element={<OrderSuccess />} />
-            <Route path="/order-fail" element={<OrderFail />} />
-            <Route path="/orders" element={<Orders />} />
-					</Routes>
-				</div>
-			)}
-		</div>
-		</SnackbarProvider>
-	);
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/bundles" element={<Bundle />} />
+              <Route path="/snacks" element={<SnacksMainPage />} />
+              <Route path="/bundles/:id" element={<BundleDetail />} />
+              <Route path="/bundles/featured" element={<BundleFeatured />} />
+              <Route path="/bundles/new" element={<BundleNew />} />
+              <Route path="/bundles/best" element={<BundleBest />} />
+              <Route path="/order-success" element={<OrderSuccess />} />
+              <Route path="/order-fail" element={<OrderFail />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        )}
+      </div>
+    </SnackbarProvider>
+  );
 };
 
 export default App;
