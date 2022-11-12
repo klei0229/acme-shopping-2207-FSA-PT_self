00610eff4ecf39aa30b088aa6c8fdbbd27adf324
Ethan Newlin <newlin5@gmail.com>
2022-11-12 08:33:25 -0500

    create address button dialog working

--- src/Components/CreateAddress.js
+++ src/Components/CreateAddress.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { loginWithToken } from '../store';
 import { Grid, Typography, TextField } from '@mui/material';
+import { loginWithToken } from '../store';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const CreateAddress = () => {
 	const { auth } = useSelector((state) => state);
@@ -17,6 +23,16 @@ const CreateAddress = () => {
 		country: '',
 	});
 
+	const [open, setOpen] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	const onChange = (ev) => {
 		setAddress({ ...address, [ev.target.name]: ev.target.value });
 	};
@@ -27,6 +43,7 @@ const CreateAddress = () => {
 		try {
 			await axios.post('/api/addresses', address);
 			await dispatch(loginWithToken());
+			handleClose();
 		} catch (ex) {
 			console.log(ex.response.data);
 		}
@@ -35,91 +52,106 @@ const CreateAddress = () => {
 	return (
 		<div>
 			<Fragment>
-				<form onSubmit={save}>
-					<Typography variant='h6' gutterBottom>
-						Create a New Address to Save to your Profile
-					</Typography>
-					<div>
-						<Grid container spacing={3}>
-							<Grid item xs={12}>
-								<TextField
-									required
-									name='label'
-									label='New Address Label'
-									fullWidth
-									variant='standard'
-									value={address.label}
-									onChange={onChange}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									required
-									name='street1'
-									label='Address line 1'
-									fullWidth
-									variant='standard'
-									value={address.street1}
-									onChange={onChange}
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									name='street2'
-									label='Address line 2'
-									fullWidth
-									variant='standard'
-									value={address.street2}
-									onChange={onChange}
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									required
-									name='city'
-									label='City'
-									fullWidth
-									variant='standard'
-									value={address.city}
-									onChange={onChange}
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									name='state'
-									label='State/Province/Region'
-									fullWidth
-									variant='standard'
-									value={address.state}
-									onChange={onChange}
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									required
-									name='zipcode'
-									label='Zipcode / Postal code'
-									fullWidth
-									variant='standard'
-									value={address.zipcode}
-									onChange={onChange}
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									required
-									name='country'
-									label='Country'
-									fullWidth
-									variant='standard'
-									value={address.country}
-									onChange={onChange}
-								/>
-							</Grid>
-						</Grid>
-					</div>
-					<button>Create New Address</button>
-				</form>
+				<Button variant='outlined' onClick={handleClickOpen}>
+					Create a new address
+				</Button>
+				<Dialog open={open} onClose={handleClose}>
+					<DialogTitle>Create a New Address</DialogTitle>
+					<DialogContent>
+						<DialogContentText>
+							To start getting tasty bundles, create an address for your
+							profile.
+						</DialogContentText>
+						<form onSubmit={save}>
+							<Typography variant='h6' gutterBottom>
+								Create a New Address to Save to your Profile
+							</Typography>
+							<div>
+								<Grid container spacing={3}>
+									<Grid item xs={12}>
+										<TextField
+											required
+											name='label'
+											label='New Address Label'
+											fullWidth
+											variant='standard'
+											value={address.label}
+											onChange={onChange}
+										/>
+									</Grid>
+									<Grid item xs={12}>
+										<TextField
+											required
+											name='street1'
+											label='Address line 1'
+											fullWidth
+											variant='standard'
+											value={address.street1}
+											onChange={onChange}
+										/>
+									</Grid>
+									<Grid item xs={12}>
+										<TextField
+											name='street2'
+											label='Address line 2'
+											fullWidth
+											variant='standard'
+											value={address.street2}
+											onChange={onChange}
+										/>
+									</Grid>
+									<Grid item xs={12} sm={6}>
+										<TextField
+											required
+											name='city'
+											label='City'
+											fullWidth
+											variant='standard'
+											value={address.city}
+											onChange={onChange}
+										/>
+									</Grid>
+									<Grid item xs={12} sm={6}>
+										<TextField
+											name='state'
+											label='State/Province/Region'
+											fullWidth
+											variant='standard'
+											value={address.state}
+											onChange={onChange}
+										/>
+									</Grid>
+									<Grid item xs={12} sm={6}>
+										<TextField
+											required
+											name='zipcode'
+											label='Zipcode / Postal code'
+											fullWidth
+											variant='standard'
+											value={address.zipcode}
+											onChange={onChange}
+										/>
+									</Grid>
+									<Grid item xs={12} sm={6}>
+										<TextField
+											required
+											name='country'
+											label='Country'
+											fullWidth
+											variant='standard'
+											value={address.country}
+											onChange={onChange}
+										/>
+									</Grid>
+								</Grid>
+							</div>
+						</form>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose}>Cancel</Button>
+						<Button onClick={save}>Create new Address</Button>
+					</DialogActions>
+				</Dialog>
 			</Fragment>
 		</div>
 	);
