62305666c4de9d84037b2db4fc5de9915f461f1f
Beejay Salting <beejay.salting@gmail.com>
2022-11-14 10:40:47 -0500

    update empty cart suggestion

--- src/Components/EmptyCart.js
+++ src/Components/EmptyCart.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Container, Typography, CssBaseline } from '@mui/material';
+import {
+  Container,
+  Typography,
+  CssBaseline,
+  Divider,
+  Grid,
+} from '@mui/material';
+import NowTrendingCard from './LandingPage/NowTrendingCard';
 
 const EmptyCart = () => {
+  const { bundles } = useSelector((state) => state);
+  const [trendingBundles, setTrendingBundles] = React.useState([]);
+
+  React.useEffect(() => {
+    if (bundles.length) {
+      const featured = bundles.filter((bundle) => bundle.type === 'featured');
+      setTrendingBundles(featured);
+      console.log(featured);
+    }
+    console.log(trendingBundles);
+  }, [bundles]);
   return (
     <Container>
       <CssBaseline />
       <div>
-        <br />
         <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
           YOUR CART IS EMPTY
         </Typography>
@@ -20,18 +38,36 @@ const EmptyCart = () => {
           We think you might like...
         </Typography>
         <br />
+
+        <Container maxWidth="xs">
+          <Divider variant="middle"></Divider>
+        </Container>
+        <br></br>
+        <Container maxWidth="xl">
+          {/* <QuiltedImageList></QuiltedImageList> */}
+          <Grid container align="center" spacing={8}>
+            {trendingBundles.map((bundle) => {
+              return (
+                <Grid item key={bundle.id} align="center" xs={3}>
+                  {/* <h1>1</h1> */}
+                  <NowTrendingCard card={bundle}></NowTrendingCard>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
         <br />
         <Link to={`/bundles`}>
           <Typography variant="subtitle1" gutterBottom sx={{ mt: 2 }}>
-            RETURN TO SHOP
+            RETURN TO BUNDLES
           </Typography>
         </Link>
         <br />
-        <Link to={`/orders`}>
+        {/* <Link to={`/orders`}>
           <Typography variant="subtitle1" gutterBottom sx={{ mt: 2 }}>
             REVIEW ORDERS
           </Typography>
-        </Link>
+        </Link> */}
       </div>
     </Container>
   );
