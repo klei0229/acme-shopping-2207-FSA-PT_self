3c5b77987b0e560418ab9a9957a1ee06789118e8
Ethan Newlin <newlin5@gmail.com>
2022-11-14 20:27:48 -0500

    updated cart useeffect

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,9 +4,6 @@ const config = {};
 if (process.env.QUIET) {
   config.logging = false;
 }
-const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db',
-  config
-);
+const conn = new Sequelize(process.env.DATABASE_URL, config);
 
 module.exports = conn;
