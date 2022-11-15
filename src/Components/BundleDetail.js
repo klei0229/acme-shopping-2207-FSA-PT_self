e906409ac507d1676e60ed65a2c1d813e8a0230c
Ethan Newlin <newlin5@gmail.com>
2022-11-14 23:18:02 -0500

    minor change

--- src/Components/BundleDetail.js
+++ src/Components/BundleDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import {
 	Grid,
 	Container,
@@ -9,10 +9,7 @@ import {
 	Button,
 	ToggleButtonGroup,
 	ToggleButton,
-<<<<<<< HEAD
-	CssBaseline,
-=======
->>>>>>> 45bd89a36937aa9dbdc8079df68062817c3b79ab
+	Link,
 } from '@mui/material';
 import SnackItemCard from './SnacksPage/SnackItemCard';
 
@@ -25,7 +22,7 @@ const BundleDetail = () => {
 	const [size, setSize] = useState('small');
 	const basePrice = 14.99;
 	const [price, setPrice] = useState(`$${basePrice}`);
-<<<<<<< HEAD
+
 	const handleSubscription = (event, newSubscription) => {
 		console.log('clicked');
 		console.log('last is:' + subscription);
@@ -48,115 +45,6 @@ const BundleDetail = () => {
 		}
 	};
 
-	const handleSize = (event, newSize) => {
-		if (newSize !== null) {
-			setSize(newSize);
-			if (newSize == 'large') {
-				if (subscription == 'monthly') {
-					setPrice(`$${(basePrice * 1.75).toFixed(2)}`);
-					console.log(1);
-				} else {
-					setPrice(`$${(basePrice * 1.75 * 11).toFixed(2)}`);
-				}
-			} else {
-				if (subscription == 'monthly') {
-					setPrice(`$${basePrice}`);
-					console.log(1);
-				} else {
-					setPrice(`$${(basePrice * 11).toFixed(2)}`);
-=======
-
-	const handleSubscription = (event, newSubscription) => {
-		console.log('clicked');
-		console.log('last is:' + subscription);
-		if (newSubscription !== null) {
-			setSubscription(newSubscription);
-			if (newSubscription == 'monthly') {
-				if (size == 'small') {
-					setPrice(`$${basePrice}`);
-				} else {
-					setPrice(`$${(basePrice * 1.75).toFixed(2)}`);
-				}
-			} else {
-				if (size == 'small') {
-					setPrice(`$${(basePrice * 1.75).toFixed(2)}`);
-					console.log(1);
-				} else {
-					setPrice(`$${(basePrice * 11 * 1.75).toFixed(2)}`);
->>>>>>> 45bd89a36937aa9dbdc8079df68062817c3b79ab
-				}
-			}
-		}
-	};
-
-<<<<<<< HEAD
-	const description =
-		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.";
-
-	const addToCart = () => {
-		console.log(subscription);
-		console.log(size);
-	};
-
-	useEffect(() => {
-		if (bundles.length) {
-			const bundle = bundles.find((bundle) => bundle.id === id);
-			setBundle(bundle);
-			setProducts(bundle.products);
-		}
-	}, [bundles]);
-
-	return (
-		<div>
-			<div>
-				<Container sx={{ py: 8 }} maxWidth='lg'>
-					<Grid container spacing={2}>
-						<Grid item md={7}>
-							<CardMedia
-								component='img'
-								height='100%'
-								image={bundle.imageUrl}
-								alt='image'
-							/>
-						</Grid>
-						<Grid item md={5}>
-							<Container
-								sx={{
-									margin: '5',
-								}}
-							>
-								<Typography variant='h3' gutterBottom>
-									{bundle.name}
-								</Typography>
-								<Typography variant='h4' gutterBottom>
-									{price}
-								</Typography>
-								<Typography variant='p' gutterBottom>
-									{description}
-								</Typography>
-								<br></br>
-								<br></br>
-								<Typography variant='h5' gutterBottom>
-									Frequency
-								</Typography>
-								<ToggleButtonGroup
-									value={subscription}
-									exclusive
-									onChange={handleSubscription}
-									aria-label='subscription'
-									fullWidth='true'
-								>
-									<ToggleButton value='monthly' aria-label='monthly'>
-										Monthly
-									</ToggleButton>
-									<ToggleButton value='annually' aria-label='annually'>
-										Annually
-									</ToggleButton>
-								</ToggleButtonGroup>
-								<br></br>
-								<br></br>
-
-=======
 	const handleSize = (event, newSize) => {
 		if (newSize !== null) {
 			setSize(newSize);
@@ -236,7 +124,6 @@ const BundleDetail = () => {
 								<br></br>
 								<br></br>
 
->>>>>>> 45bd89a36937aa9dbdc8079df68062817c3b79ab
 								<Typography variant='h5'>Size</Typography>
 								<ToggleButtonGroup
 									value={size}
@@ -265,8 +152,8 @@ const BundleDetail = () => {
 								</Button>
 								<Button sx={{ mt: '10' }} variant='contained' fullWidth='true'>
 									<Link
-										to={'/bundles'}
-										style={{ textDecoration: 'none', color: 'white' }}
+										href={'#/bundles'}
+										sx={{ textDecoration: 'none', color: 'black' }}
 									>
 										Return to our other bundles
 									</Link>
